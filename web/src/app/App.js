653eb0e7f00dc6c4683de1aa209571bf31fa1480
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-09-19 13:41:55 +0200

    Restructure App

--- web/src/app/App.js
+++ web/src/app/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './assets/logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+import Sidebar from '../sidebar/Sidebar'
+import Header from '../header/Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Sidebar />
+        <Header />
       </div>
     );
   }
 }
 
-export default App;
+export default App
