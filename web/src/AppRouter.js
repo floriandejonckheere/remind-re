081fb583e2f6dd516c0cb7e7c51e643d3035d3e6
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-09-19 14:34:36 +0200

    Remove landing route

--- web/src/AppRouter.js
+++ web/src/AppRouter.js
@@ -3,7 +3,6 @@ import { Route, Switch } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/App';
-import Landing from './landing/Landing';
 
 class AppRouter extends Component {
   render() {
@@ -11,7 +10,6 @@ class AppRouter extends Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={App}/>
-          <Route path="/landing" component={Landing}/>
         </Switch>
       </BrowserRouter>
     )
