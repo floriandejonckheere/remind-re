20707d828dfa1be91559dc197c980cfdb0a63eee
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-09-20 13:28:05 +0200

    Add Font Awesome

--- web/src/index.js
+++ web/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'uikit/dist/js/uikit.min'
-import 'uikit/dist/js/uikit-icons.min'
+import 'uikit/dist/js/uikit'
+
+import 'typeface-montserrat'
+
+import 'font-awesome/fonts/FontAwesome.otf'
+import 'font-awesome/fonts/fontawesome-webfont.eot'
+import 'font-awesome/fonts/fontawesome-webfont.svg'
+import 'font-awesome/fonts/fontawesome-webfont.ttf'
+import 'font-awesome/fonts/fontawesome-webfont.woff'
+import 'font-awesome/fonts/fontawesome-webfont.woff2'
+import 'font-awesome/css/font-awesome.css'
 
 import './index.css';
-import 'typeface-montserrat'
 
 import registerServiceWorker from './registerServiceWorker';
 
