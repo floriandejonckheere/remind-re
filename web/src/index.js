0f13e2bfb88aa2eae1c7f8932397f3a53fdad037
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-09-20 09:07:54 +0200

    Set Montserrat as default font

--- web/src/index.js
+++ web/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'uikit/dist/js/uikit'
-import 'uikit/dist/js/uikit-icons'
+import 'uikit/dist/js/uikit.min'
+import 'uikit/dist/js/uikit-icons.min'
 
 import './index.css';
+import 'typeface-montserrat'
+
 import registerServiceWorker from './registerServiceWorker';
 
 import AppRouter from './AppRouter';
