ce554aaf94af42dd95a3a5f1b79d8e5e8b5009b2
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-09-22 10:35:45 +0200

    Remove Font Awesome

--- web/src/index.js
+++ web/src/index.js
@@ -6,14 +6,6 @@ import Icons from 'uikit/dist/js/uikit-icons'
 
 import 'typeface-montserrat'
 
-import 'font-awesome/fonts/FontAwesome.otf'
-import 'font-awesome/fonts/fontawesome-webfont.eot'
-import 'font-awesome/fonts/fontawesome-webfont.svg'
-import 'font-awesome/fonts/fontawesome-webfont.ttf'
-import 'font-awesome/fonts/fontawesome-webfont.woff'
-import 'font-awesome/fonts/fontawesome-webfont.woff2'
-import 'font-awesome/css/font-awesome.css'
-
 import './index.css';
 
 import registerServiceWorker from './registerServiceWorker';
