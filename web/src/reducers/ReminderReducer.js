32bd421bfae5b00aef75c935434188dbfa222d06
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-09-22 16:24:22 +0200

    Split up reducers

---
+++ web/src/reducers/ReminderReducer.js
@@ -0,0 +1,48 @@
+import Immutable from 'seamless-immutable'
+
+import * as ActionTypes from 'actions/ActionTypes'
+
+const initialState = Immutable({
+  reminderId: 0,
+  reminders: []
+})
+
+/**
+ * Add a reminder
+ * @param state
+ * @param action
+ */
+function addReminder(state, action) {
+  return Immutable.merge(state, {
+    reminderId: state.reminderId + 1,
+    reminders: {
+      [state.reminderId]: action.data
+    }
+  });
+}
+
+/**
+ * Remove a reminder
+ * @param state
+ * @param action
+ */
+function deleteReminder(state, action) {
+  return Immutable(state).delete(action.id)
+}
+
+/**
+ * Root reducer
+ * @param state
+ * @param action
+ * @returns {{type, id}|*}
+ */
+function ReminderReducer(state = initialState, action) {
+  switch (action.type) {
+    case ActionTypes.ADD_REMINDER: return addReminder(state, action)
+    case ActionTypes.REMOVE_REMINDER: return removeReminder(state, action)
+    default:
+      return state
+  }
+}
+
+export default ReminderReducer
--- web/src/reducers/RootReducer.js
+++ web/src/reducers/RootReducer.js
@@ -1,49 +1,11 @@
+import { combineReducers } from 'redux'
+
 import Immutable from 'seamless-immutable'
 
-import * as ActionTypes from 'actions/ActionTypes'
+import ReminderReducer from 'reducers/ReminderReducer'
 
-const initialState = Immutable({
-  id: 0,
-  reminders: [],
-  labels: []
+const reminderApp = combineReducers({
+  ReminderReducer
 })
-
-/**
- * Add a reminder
- * @param state
- * @param action
- */
-function addReminder(state, action) {
-  return Immutable.merge(state, {
-    id: state.id + 1,
-    reminders: {
-      [state.id]: action.data
-    }
-  });
-}
-
-/**
- * Remove a reminder
- * @param state
- * @param action
- */
-function deleteReminder(state, action) {
-  return Immutable(state).delete(action.id)
-}
-
-/**
- * Root reducer
- * @param state
- * @param action
- * @returns {{type, id}|*}
- */
-function reminderApp(state = initialState, action) {
-  switch (action.type) {
-    case ActionTypes.ADD_REMINDER: return addReminder(state, action)
-    case ActionTypes.REMOVE_REMINDER: return removeReminder(state, action)
-    default:
-      return state
-  }
-}
 
 export default reminderApp
