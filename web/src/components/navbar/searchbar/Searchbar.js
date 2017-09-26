b0ef944ff6299c0e05105c1932d426662fe12e3c
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-09-26 12:43:01 +0200

    Responsive Entry

--- web/src/components/navbar/searchbar/Searchbar.js
+++ web/src/components/navbar/searchbar/Searchbar.js
@@ -6,7 +6,7 @@ class Searchbar extends Component {
   render() {
     return (
       <div className="re-searchbar uk-width-expand">
-        <input type="text" className="uk-input" placeholder="Quick search" />
+        <input type="search" className="uk-input uk-search-input" placeholder="Quick search" />
       </div>
     )
   }
--- web/src/components/reminderlist/entry/Entry.js
+++ web/src/components/reminderlist/entry/Entry.js
@@ -8,11 +8,11 @@ class Entry extends Component {
     return (
       <li className="uk-padding-small">
         <div className="uk-grid">
-          <div className="uk-flex uk-flex-middle">
+          <div className="uk-flex uk-flex-middle uk-hidden">
             <input type="checkbox" className="uk-checkbox" />
           </div>
           <div className="uk-width-expand">
-            <div className="uk-text-lead">
+            <div className="uk-text-lead uk-text-truncate">
               { this.props.data.active ?
                 this.props.data.title
               : (
@@ -22,11 +22,21 @@ class Entry extends Component {
                 <span className="uk-margin-left uk-text-muted" title="Recurring reminder" data-uk-icon="icon: future; ratio: .8" data-uk-tooltip />
               }
             </div>
-            <p className="uk-text-meta uk-margin-remove-top">
-              { moment(this.props.data.due).fromNow() }
-            </p>
+            <div className="uk-grid uk-flex uk-flex-between">
+              <div className="uk-flex uk-flex-middle">
+                <p className="uk-text-meta uk-margin-remove-top">
+                  { moment(this.props.data.due).fromNow() }
+                </p>
+              </div>
+              <div className="uk-flex-right uk-hidden@m">
+                <ul className="uk-navbar-nav">
+                  <li><a href="#" title="Edit" data-uk-icon="icon: pencil" data-uk-tooltip /></li>
+                  <li><a href="#" title="Delete" data-uk-icon="icon: close" data-uk-tooltip /></li>
+                </ul>
+              </div>
+            </div>
           </div>
-          <div className="uk-flex uk-flex-middle">
+          <div className="uk-flex uk-flex-middle uk-visible@m">
             <ul className="uk-iconnav">
               <li><a href="#" className="uk-button" data-uk-icon="icon: more" /></li>
               <div className="uk-nav uk-dropdown-nav uk-padding-small" data-uk-dropdown="mode: click; pos: bottom-left">
