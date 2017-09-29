a4127944946e15f6b883dc1953243d90c94f881c
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-09-29 09:56:39 +0200

    Rename FilterEntry to EntryContainer, extract FilterList

---
+++ src/components/sidebar/FilterList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+import * as Constants from 'actions/Constants'
+import EntryContainer from 'components/sidebar/entry/EntryContainer'
+
+class FilterList extends Component {
+  render() {
+    return (
+      <div>
+        <EntryContainer filter={Constants.FILTER_ALL}>
+          all reminders
+        </EntryContainer>
+        <EntryContainer filter={Constants.FILTER_UPCOMING}>
+          upcoming
+        </EntryContainer>
+        <EntryContainer filter={Constants.FILTER_NEXT_WEEK}>
+          next week
+        </EntryContainer>
+      </div>
+    )
+  }
+}
+
+export default FilterList
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,34 +1,15 @@
 import React, { Component } from 'react'
 
-import * as Constants from 'actions/Constants'
-import FilterEntry from 'components/sidebar/entry/FilterEntry'
+import FilterList from 'components/sidebar/FilterList'
+import LabelList from 'components/sidebar/LabelList'
 
 class Sidebar extends Component {
   render() {
     return (
       <div className="uk-padding">
         <ul className="uk-nav uk-nav-default">
-          <FilterEntry filter={Constants.FILTER_ALL}>
-            all reminders
-          </FilterEntry>
-          <FilterEntry filter={Constants.FILTER_UPCOMING}>
-            upcoming
-          </FilterEntry>
-          <FilterEntry filter={Constants.FILTER_NEXT_WEEK}>
-            {console.log(Constants)}
-            next week
-          </FilterEntry>
-          <div className="uk-margin-large-top" />
-          {this.props.labels.map(label => (
-            <FilterEntry filter={`FILTER_LABEL_${label.id}`} key={label.id}>
-              {label.title}
-            </FilterEntry>
-          ))}
-          <li>
-            <a href="#" className="uk-text-primary">
-              add label
-            </a>
-          </li>
+          <FilterList />
+          <LabelList />
         </ul>
       </div>
     )
