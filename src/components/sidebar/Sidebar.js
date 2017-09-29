e33bb7fca39b26577e9b4f585fffc2fce6fdb205
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-09-29 09:35:54 +0200

    Map labels

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -5,16 +5,6 @@ import FilterEntry from 'components/sidebar/entry/FilterEntry'
 
 class Sidebar extends Component {
   render() {
-    let rows = []
-
-    this.props.labels.forEach((label) => {
-      rows.push(
-        <FilterEntry filter={`FILTER_LABEL_${label.id}`} key={label.id}>
-          {label.title}
-        </FilterEntry>
-       )
-     })
-
     return (
       <div className="uk-padding">
         <ul className="uk-nav uk-nav-default">
@@ -29,7 +19,11 @@ class Sidebar extends Component {
             next week
           </FilterEntry>
           <div className="uk-margin-large-top" />
-          {rows}
+          {this.props.labels.map(label => (
+            <FilterEntry filter={`FILTER_LABEL_${label.id}`} key={label.id}>
+              {label.title}
+            </FilterEntry>
+          ))}
           <li>
             <a href="#" className="uk-text-primary">
               add label
