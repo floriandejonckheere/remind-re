0a8d889f99cda588ebae20a62c254f6f95a14662
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-10-01 14:18:25 +0200

    Add hover delete icon to labels

--- src/components/sidebar/entry/Entry.js
+++ src/components/sidebar/entry/Entry.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+import './Entry.css'
+
 class Entry extends Component {
   render() {
     return (
-      <li className={this.props.active ? 'uk-active' : ''}>
+      <li className={this.props.active ? 're-sidebar-label-entry uk-active' : 're-sidebar-label-entry'}>
+        {this.props.canDelete && (
+          <div>
+            <a
+              href="#"
+              className="re-delete-icon uk-float-right uk-button"
+              data-uk-icon="icon: more; ratio: .8" />
+            <div className="uk-nav uk-dropdown-nav uk-padding-small" data-uk-dropdown="mode: click; pos: bottom-left">
+              <ul className="uk-nav uk-nav-default">
+                <li><a href="#"><span className="uk-link-icon" data-uk-icon="icon: pencil" /> Edit</a></li>
+                <li><a href="#" onClick={this.props.onClickDelete}><span className="uk-link-icon" data-uk-icon="icon: trash" /> Delete</a></li>
+              </ul>
+            </div>
+          </div>
+        )}
         <a
           href="#"
           className="uk-margin"
