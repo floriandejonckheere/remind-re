2262e26db55915d1fc000e82526c166617c7c7a5
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-10-05 11:01:24 +0200

    Rewrite Entry

--- src/components/list/entry/Entry.js
+++ src/components/list/entry/Entry.js
@@ -16,19 +16,11 @@ class Entry extends Component {
   }
 
   render() {
-    return (
-      <li className="re-reminder-entry uk-padding-small">
-        <div className="uk-grid">
-          {this.state.editing || (
-            <div className="uk-padding-remove">
-              <a
-                className="re-hover-icon re-drag-icon uk-float-right uk-button uk-padding-remove-right"
-                data-uk-icon="icon: menu"
-              />
-            </div>
-          )}
-          <div className="uk-width-expand">
-            {this.state.editing ? (
+    if (this.state.editing) {
+      return (
+        <li className="re-reminder-entry uk-padding-small">
+          <div className="uk-grid">
+            <div className="uk-width-expand">
               <form className="uk-margin-top" onSubmit={this.onClickSubmit}>
                 <div className="uk-margin">
                   <input
@@ -75,48 +67,59 @@ class Entry extends Component {
                   ))}
                 </div>
               </form>
-            ) : (
-              <div>
-                <div className="uk-text-lead uk-text-truncate no-drag">
-                  {this.props.data.title ? (
-                    this.props.data.title
-                  ) : (
-                    <em className="uk-text-muted">Untitled reminder</em>
-                  )}
-                  {this.props.data.interval !== null &&
-                    <span className="uk-margin-left uk-text-muted" title="Recurring reminder" data-uk-icon="icon: future; ratio: .8" data-uk-tooltip />
-                  }
+            </div>
+          </div>
+        </li>
+      )
+    } else {
+      return (
+        <li className="re-reminder-entry uk-padding-small">
+          <div className="uk-grid">
+            <div className="uk-padding-remove">
+              <a
+                className="re-hover-icon re-drag-icon uk-float-right uk-button uk-padding-remove-right"
+                data-uk-icon="icon: menu"
+              />
+            </div>
+            <div className="uk-width-expand">
+              <div className="uk-text-lead uk-text-truncate no-drag">
+                {this.props.data.title ? (
+                  this.props.data.title
+                ) : (
+                  <em className="uk-text-muted">Untitled reminder</em>
+                )}
+                {this.props.data.interval !== null &&
+                <span className="uk-margin-left uk-text-muted" title="Recurring reminder" data-uk-icon="icon: future; ratio: .8" data-uk-tooltip />
+                }
+              </div>
+              <div className="uk-grid uk-flex uk-flex-between">
+                <div className="uk-flex uk-flex-middle">
+                  <p className="uk-text-meta uk-margin-remove-top no-drag">
+                    {moment(this.props.data.due).fromNow()}
+                    {this.props.data.labels.map(id => (
+                      <LabelContainer
+                        id={id}
+                        key={id}
+                        active="true"
+                        onClick={this.props.onClickLabel}
+                      />
+                    ))}
+                  </p>
                 </div>
-                <div className="uk-grid uk-flex uk-flex-between">
-                  <div className="uk-flex uk-flex-middle">
-                    <p className="uk-text-meta uk-margin-remove-top no-drag">
-                      {moment(this.props.data.due).fromNow()}
-                      {this.props.data.labels.map(id => (
-                        <LabelContainer
-                          id={id}
-                          key={id}
-                          active="true"
-                          onClick={this.props.onClickLabel}
-                        />
-                      ))}
-                    </p>
-                  </div>
-                  <div className="uk-flex-right uk-hidden@m">
-                    <ul className="uk-navbar-nav">
-                      <li><a title="Trigger" data-uk-icon="icon: bell" data-uk-tooltip data-uk-toggle="target: #modal-full" onClick={this.props.onClickTrigger} /></li>
-                      <li><a title="Edit" data-uk-icon="icon: pencil" data-uk-tooltip onClick={this.onClickEdit} /></li>
-                      <li><a title="Delete" data-uk-icon="icon: close" data-uk-tooltip onClick={this.props.onClickDelete} /></li>
-                    </ul>
-                  </div>
+                <div className="uk-flex-right uk-hidden@m">
+                  <ul className="uk-navbar-nav">
+                    <li><a title="Trigger" data-uk-icon="icon: bell" data-uk-tooltip data-uk-toggle="target: #modal-full" onClick={this.props.onClickTrigger} /></li>
+                    <li><a title="Edit" data-uk-icon="icon: pencil" data-uk-tooltip onClick={this.onClickEdit} /></li>
+                    <li><a title="Delete" data-uk-icon="icon: close" data-uk-tooltip onClick={this.props.onClickDelete} /></li>
+                  </ul>
                 </div>
               </div>
-            )}
-          </div>
-          {this.state.editing || (
+            </div>
             <div className="uk-flex uk-flex-middle uk-visible@m">
               <a
                 className="re-hover-icon uk-float-right uk-button"
-                data-uk-icon="icon: more; ratio: .8" />
+                data-uk-icon="icon: more; ratio: .8"
+              />
               <div className="uk-nav uk-dropdown-nav uk-padding-small" data-uk-dropdown="mode: click; pos: bottom-left">
                 <ul className="uk-nav uk-nav-default">
                   <li><a onClick={this.props.onClickTrigger} data-uk-toggle="target: #modal-full"><span className="uk-link-icon" data-uk-icon="icon: bell" /> Trigger</a></li>
@@ -125,10 +128,10 @@ class Entry extends Component {
                 </ul>
               </div>
             </div>
-          )}
-        </div>
-      </li>
-    )
+          </div>
+        </li>
+      )
+    }
   }
 
   onClickEdit() {
